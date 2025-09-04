bd50b7d280ae96ba74d4cf86018d0a7b7ccc6674
fazaa-nj <faza_kamaliah_34rpl@student.smktelkom-mlg.sch.id>
2025-09-04 17:16:33 +0700

    Add files via upload

---
+++ PrisonerTest.java
@@ -0,0 +1,8 @@
+public class PrisonerTest {
+  public static void main(String[] args) {
+    Prisoner bubba = new Prisoner ();
+    Prisoner twitch = new Prisoner();
+
+    System.out.println(bubba == twitch); //false
+  }  // end method main
+} // end class PrisonerTest
